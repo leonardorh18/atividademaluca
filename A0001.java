63d5b660694796d623b99ab41e62a180dcebecc1
Douglas Kosvoski <douglas.contactpro@gmail.com>
2020-11-18 14:13:39 -0300

    transforma maior e menor em funcoes

--- A0001.java
+++ A0001.java
@@ -1,16 +1,31 @@
 class Main {
-  public static void main(String[] args) {
-    int armazena[] = new int[20];
-	int maior = -9999999;
-	int menor = 99999999;
+	public static void main(String[] args) {
+	int armazena[] = new int[20];
 
-	for (int i = 0; i < armazena.length; i++) {
-		if(armazena[i] > maior) {
-			maior = armazena[i];
-		}
-		if(armazena[i] < menor) {
-			menor = armazena[i];
-		}
+	System.out.println(maior(armazena));
+	System.out.println(menor(armazena));
+
 	}
-  }
+
+	public static int maior(int[] lista) {
+		int maior = -9999999;
+
+		for (int i = 0; i < lista.length; i++) {
+			if(lista[i] > maior) {
+				maior = lista[i];
+			}
+		}
+		return maior;
+	}
+
+	public static int menor(int[] lista) {
+		int menor = 9999999;
+
+		for (int i = 0; i < lista.length; i++) {
+			if(lista[i] < menor) {
+				menor = lista[i];
+			}
+		}
+		return menor;
+	}
 }
