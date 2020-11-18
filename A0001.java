a41dd3f4cf7386fff011bc07b37fca6060f95581
Douglas Kosvoski <douglas.contactpro@gmail.com>
2020-11-18 14:08:13 -0300

    encontra maior e menor

--- A0001.java
+++ A0001.java
@@ -3,5 +3,14 @@ class Main {
     int armazena[] = new int[20];
 	int maior = -9999999;
 	int menor = 99999999;
+
+	for (int i = 0; i < armazena.length; i++) {
+		if(armazena[i] > maior) {
+			maior = armazena[i];
+		}
+		if(armazena[i] < menor) {
+			menor = armazena[i];
+		}
+	}
   }
 }
